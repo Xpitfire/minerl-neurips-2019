feb89c197ada7ee1e920ec03b72bffa8d1961268
Xpitfire <dinu.marius-constantin@hotmail.com>
2020-03-10 09:28:08 +0100

    Removed old files

--- bc_test.py
+++
@@ -1,6 +0,0 @@
-from src.bc.runner import Runner
-
-
-if __name__ == "__main__":
-    runner = Runner()
-    runner.run()
--- global_params.py
+++
@@ -1,9 +0,0 @@
-class Args(object):
-    def __init__(self):
-        self.parameter = []
-        self.enable_logging = True
-        self.log_steps = 10
-
-
-global args
-args = Args()
